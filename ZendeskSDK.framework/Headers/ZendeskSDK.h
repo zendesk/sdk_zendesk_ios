9e003713fa8aa2fa29908d555d15937017ee6b56
runner <runner@Mac-1638798413069.local>
2021-12-06 14:02:23 +0000

    release/1.0.0

---
+++ ZendeskSDK.framework/Headers/ZendeskSDK.h
@@ -0,0 +1,26 @@
+//
+// Zendesk.h
+// Zendesk
+//
+// Created by Zendesk on 17/09/2021.
+//
+// 
+//
+// By downloading or using the Zendesk Mobile SDK, You agree to the Zendesk Master
+// Subscription Agreement https://www.zendesk.com/company/customers-partners/#master-subscription-agreement
+// and Application Developer and API License
+//
+// Agreement https://www.zendesk.com/company/customers-partners/#application-developer-api-license-agreement and
+// acknowledge that such terms govern Your use of and access to the Mobile SDK.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Zendesk.
+FOUNDATION_EXPORT double ZendeskVersionNumber;
+
+//! Project version string for Zendesk.
+FOUNDATION_EXPORT const unsigned char ZendeskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Zendesk/PublicHeader.h>
+
+
